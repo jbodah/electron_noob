08c9fdc51695faa2f77d45f92fe281417d1ebae0
Josh Bodah <jb3689@yahoo.com>
2017-06-16 15:58:03 -0400

    focus modal button

--- windows/renderer.js
+++ windows/renderer.js
@@ -3,6 +3,7 @@ const BrowserWindow = remote.BrowserWindow
 const path = require('path')
 
 const newWindowBtn = document.getElementById('new-window')
+const focusBtn = document.getElementById('focus-modal')
 
 newWindowBtn.addEventListener('click', event => {
   console.log('test')
@@ -10,14 +11,32 @@ newWindowBtn.addEventListener('click', event => {
   let win = new BrowserWindow({width: 400, height: 320})
   win.loadURL(modalPath)
   win.show()
+
+  // Focus/Blur events
+  win.on('focus', hideFocusBtn)
+  win.on('blur', showFocusBtn)
+  function showFocusBtn (btn) {
+    if (!win) return
+    focusBtn.classList.add('smooth-appear')
+    focusBtn.classList.remove('disappear')
+    focusBtn.addEventListener('click', clickHandler)
+  }
+  function hideFocusBtn () {
+    focusBtn.classList.add('disappear')
+    focusBtn.classList.remove('smooth-appear')
+    focusBtn.removeEventListener('click', clickHandler)
+  }
+  function clickHandler () {
+    win.focus()
+  }
 })
 
+// Move/Resize events
 let win = remote.getCurrentWindow()
 win.on('resize', updateWindowStats)
 win.on('move', updateWindowStats)
 
 function updateWindowStats () {
-  console.log('yo')
   const windowStats = document.getElementById('window-stats')
   const msg = `Size: ${win.getSize()} Pos: ${win.getPosition()}`
   windowStats.innerText = msg
